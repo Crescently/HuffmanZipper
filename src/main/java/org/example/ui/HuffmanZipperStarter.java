32cd5195ace699c3d9f9110e5564db17d4a7e350
Crescentlymon <2376355613@qq.com>
2024-07-06 20:38:48 +0800

    Fixed 修复了依赖引入

--- src/main/java/org/example/ui/HuffmanZipperStarter.java
+++ src/main/java/org/example/ui/HuffmanZipperStarter.java
@@ -5,13 +5,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Objects;
 
+@Slf4j
 public class HuffmanZipperStarter extends Application {
-    private static final Logger log = LoggerFactory.getLogger(HuffmanZipperStarter.class);
 
     public static void main(String[] args) {
         // 启动UI界面
